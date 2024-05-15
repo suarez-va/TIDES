b20c79ac72075efbd7d834599be17395a6eb53b4
mc-rohan <matthew.c.rohan@gmail.com>
2024-05-15 00:34:32 -0400

    Added abs_spec feature
    Replicated NWChem's RTTDDFT water absorption example

---
+++ src/examples/water_absorption.py
@@ -0,0 +1,27 @@
+import numpy as np
+from pyscf import gto, scf, dft
+import rt_scf
+import rt_spec
+
+mol = gto.M(
+	verbose = 0,
+	atom='''
+  O     0.00000000    -0.00001441    -0.34824012
+  H    -0.00000000     0.76001092    -0.93285191
+  H     0.00000000    -0.75999650    -0.93290797
+  ''',
+	basis='6-31G',
+    spin = 0)
+
+mf = dft.RKS(mol)
+mf.xc = 'PBE0'
+mf.kernel()
+
+rt_mf = rt_scf.rt_scf(mf, 0.2, 1, 1000, 'water_abs')
+
+rt_mf.delta_field = [0.0001,0.0001,0.0001]
+rt_mf.prop = 'magnus_interpol'
+
+rt_mf.kernel()
+
+rt_spec.abs_spec(rt_mf,0.0001,50000,50) # Zero-pad and exponential damping for clean spectrum
