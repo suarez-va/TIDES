900f021a4b44b9c1fb75b28a8f3a891b5a88ab4c
mc-rohan <matthew.c.rohan@gmail.com>
2024-07-18 23:44:06 -0400

    Added back examples
    Botched last few commits, accidently deleted/moved files

--- src/examples/h_atom.py
+++ src/examples/h_atom.py
@@ -16,7 +16,7 @@ mf = scf.ghf.GHF(mol)
 mf.kernel()
 
 static_bfield(mf, [0,0,mag_z])
-rt_mf = rt_scf.RT_SCF(mf, 20, 1, 10000, 'h_atom')
+rt_mf = rt_scf.RT_SCF(mf, 1, 1, 10000)
 rt_mf.observables.update(mag=True)
 
 rt_mf.kernel()
---
+++ src/examples/li_atom.py
@@ -0,0 +1,31 @@
+import numpy as np
+from pyscf import gto, scf, dft
+from rt_scf import RT_SCF
+from staticfield import static_bfield
+from rt_utils import input_fragments
+
+mag = -0.000085 # in au
+
+mag_x = mag * np.sin(np.pi/4) * np.cos(np.pi/4)
+mag_y = mag * np.sin(np.pi/4) * np.sin(np.pi/4)
+mag_z = mag * np.cos(np.pi/4) 
+
+LiMol = gto.M(
+	verbose = 0,
+	atom='''
+ Li                 0.00000000    0.00000000 0.00000000
+''',
+	basis='3-21G',
+    spin = 1)
+
+
+Li = scf.ghf.GHF(LiMol)
+
+Li.kernel()
+
+static_bfield(Li, [mag_x,mag_y,mag_z])
+rt_mf = RT_SCF(Li, 1, 1, 10000)
+
+rt_mf.observables.update(dipole=True, mag=True)
+rt_mf.kernel()
+
