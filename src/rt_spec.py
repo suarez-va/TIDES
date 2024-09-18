1037196cdfd0bcd800d46b54474896ecad4df283
mc-rohan <matthew.c.rohan@gmail.com>
2024-09-18 16:26:09 -0400

    typos, update rt_spec
    Some logging typos
    rt_spec more general, works for quadrupoles

--- src/rt_spec.py
+++ src/rt_spec.py
@@ -8,35 +8,26 @@ Real-time SCF Spectra
 '''
 
 c = lib.param.LIGHT_SPEED
-
-def abs_spec(time, dipole, filename, kick_str=1, pad=None, damp=None, preprocess_zero=True):
+def abs_spec(time, pole, filename, kick_str=1, pad=None, damp=None, preprocess_zero=True):
     '''
     Performs 1D Fourier Transform on time-dependent dipole moment.
     Adapted from NWChem's fft1d.m GNU Octave script (Kenneth Lopata), which can be found at https://nwchemgit.github.io/RT-TDDFT.html#absorption-spectrum-of-water
     '''
 
-    dipolex_t = np.copy(dipole[:,0])
-    dipoley_t = np.copy(dipole[:,1])
-    dipolez_t = np.copy(dipole[:,2])
+    pole_t = np.copy(pole)
 
     if preprocess_zero:
-        dipolex_t -= dipolex_t[0]
-        dipoley_t -= dipoley_t[0]
-        dipolez_t -= dipolez_t[0]
+        pole_t -= pole_t[0,:]
 
     if damp:
         d = np.exp((-1 * (time - time[0])) / damp)
-        dipolex_t *= d
-        dipoley_t *= d
-        dipolez_t *= d
+        pole_t *= d[:, np.newaxis]
 
     if pad:
-        zeros = np.linspace(0,0, pad)
-        dipolex_t = np.append(dipolex_t,zeros)
-        dipoley_t = np.append(dipoley_t,zeros)
-        dipolez_t = np.append(dipolez_t,zeros)
+        zeros = np.zeros((pole_t.shape[1], pad))
+        pole_t = np.row_stack((pole_t, zeros.T))
 
-    n = len(dipolex_t)
+    n = pole_t.shape[0]
     dt = time[1] - time[0]              # Assumes constant timestep
     period = (n-1) * dt - time[0]
     dw = 2.0 * np.pi / period
@@ -45,13 +36,13 @@ def abs_spec(time, dipole, filename, kick_str=1, pad=None, damp=None, preprocess
     wmin = 0.0
     wmax = m * dw
     w = np.linspace(wmin, wmax, m)
-    dipolex_f = fft(dipolex_t)
-    dipoley_f = fft(dipoley_t)
-    dipolez_f = fft(dipolez_t)
 
-    im_dipole_f = np.imag(dipolex_f) + np.imag(dipoley_f) + np.imag(dipolez_f)
-    
-    im_dipole_f = np.abs(im_dipole_f[:m])
-    osc_str = (4 * np.pi) / (3 * c * kick_str) * w * im_dipole_f
+    im_pole_f = np.zeros(len(pole_t[:,0]))
+    for i in range(pole_t.shape[1]):
+        pole_f = fft(pole_t[:,i])
+        im_pole_f += np.imag(pole_f)
+
+    im_pole_f = np.abs(im_pole_f[:m])
+    osc_str = (4 * np.pi) / (3 * c * kick_str) * w * im_pole_f
     abs_vs_freq = np.transpose([w,osc_str])
     np.savetxt(filename + '.txt', abs_vs_freq)
