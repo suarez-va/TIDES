5b90602b9eb79935bb88c7f8e41ad9b6f2922be4
suarez-va <suarez.a.victor@gmail.com>
2025-03-11 11:25:25 -0400

    Matts first ZORA commit

---
+++ src/examples/ZORA/Ti.py
@@ -0,0 +1,27 @@
+from pyscf import gto, scf, dft
+from tides import rt_scf, rt_vapp
+from tides.zora.relativistic import ZORA
+from sapporo import sapporo
+
+# Build mol
+mol = gto.M(
+	verbose = 0,
+	atom='Ti 0 0 0',
+	basis=sapporo,
+    charge=4)
+
+# Build and run GHF object
+ti = dft.GKS(mol)
+ti.xc = 'PBE0'
+
+ti.kernel()
+
+print('SCF ENERGY BEFORE ZORA (AU): ', ti.e_tot)
+zora_obj = ZORA(ti)
+Hcore = zora_obj.get_zora_correction()
+
+ti.get_hcore = lambda *args: Hcore
+
+ti.kernel()
+
+print('SCF ENERGY AFTER ZORA (AU): ', ti.e_tot)
