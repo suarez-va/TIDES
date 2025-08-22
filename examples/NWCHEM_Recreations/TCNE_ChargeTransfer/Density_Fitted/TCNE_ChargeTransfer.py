f6e0f4aac62a8e98458a05869dcdd247cea1be61
mc-rohan <matthew.c.rohan@gmail.com>
2025-08-22 14:16:27 -0400

    TCNE Density fitting example
    Used PySCF default density fitting to run a TCNE calculation that more closely matches NWChem's example

---
+++ examples/NWCHEM_Recreations/TCNE_ChargeTransfer/Density_Fitted/TCNE_ChargeTransfer.py
@@ -0,0 +1,106 @@
+from pyscf import gto, dft, scf
+import numpy as np
+from tides import rt_scf, rt_utils, basis_utils
+
+
+'''
+Recreated from:
+https://nwchemgit.github.io/RT-TDDFT.html#charge-transfer-between-a-tcne-dimer
+'''
+
+
+# First build and run SCF for the TCNE dimer as well as the top/bottom TCNE monomers
+dimer = gto.Mole()
+top = gto.Mole()
+bottom = gto.Mole()
+
+dimer.atom = '''
+ C    -1.77576486     0.66496556     0.00004199
+ N    -2.94676621     0.71379797     0.00004388
+ C    -0.36046718     0.62491168     0.00003506
+ C     0.36049301    -0.62492429    -0.00004895
+ C     1.77579907    -0.66504145    -0.00006082
+ N     2.94680364    -0.71382258    -0.00006592
+ C    -0.31262746    -1.87038951    -0.00011201
+ N    -0.85519492    -2.90926164    -0.00016331
+ C     0.31276207     1.87031662     0.00010870
+ N     0.85498782     2.90938919     0.00016857
+#---
+ C    -1.77576486     0.66496556     3.00004199
+ N    -2.94676621     0.71379797     3.00004388
+ C    -0.36046718     0.62491168     3.00003506
+ C     0.36049301    -0.62492429     2.99995105
+ C     1.77579907    -0.66504145     2.99993918
+ N     2.94680364    -0.71382258     2.99993408
+ C    -0.31262746    -1.87038951     2.99988799
+ N    -0.85519492    -2.90926164     2.99983669
+ C     0.31276207     1.87031662     3.00010870
+ N     0.85498782     2.90938919     3.00016857
+'''
+
+top.atom = '''
+ C    -1.77576486     0.66496556     3.00004199
+ N    -2.94676621     0.71379797     3.00004388
+ C    -0.36046718     0.62491168     3.00003506
+ C     0.36049301    -0.62492429     2.99995105
+ C     1.77579907    -0.66504145     2.99993918
+ N     2.94680364    -0.71382258     2.99993408
+ C    -0.31262746    -1.87038951     2.99988799
+ N    -0.85519492    -2.90926164     2.99983669
+ C     0.31276207     1.87031662     3.00010870
+ N     0.85498782     2.90938919     3.00016857
+'''
+
+
+bottom.atom = '''
+ C    -1.77576486     0.66496556     0.00004199
+ N    -2.94676621     0.71379797     0.00004388
+ C    -0.36046718     0.62491168     0.00003506
+ C     0.36049301    -0.62492429    -0.00004895
+ C     1.77579907    -0.66504145    -0.00006082
+ N     2.94680364    -0.71382258    -0.00006592
+ C    -0.31262746    -1.87038951    -0.00011201
+ N    -0.85519492    -2.90926164    -0.00016331
+ C     0.31276207     1.87031662     0.00010870
+ N     0.85498782     2.90938919     0.00016857
+'''
+
+dimer.basis = '3-21++G'
+top.basis = '3-21++G'
+bottom.basis = '3-21++G'
+
+dimer.charge = -1
+dimer.spin = 1
+dimer.build()
+
+top.charge = 0
+top.spin = 0
+bottom.charge = -1
+bottom.spin = 1
+top.build()
+bottom.build()
+
+# Using PySCF's default density fitting
+dimer = scf.UKS(dimer).density_fit()
+top = scf.UKS(top).density_fit()
+bottom = scf.UKS(bottom).density_fit()
+
+dimer.xc = 'CAMB3LYP'
+top.xc = 'CAMB3LYP'
+bottom.xc = 'CAMB3LYP'
+
+dimer.kernel()
+top.kernel()
+bottom.kernel()
+
+
+# Now overwrite the dimer orbitals with the SCF orbitals of the bottom/top monomers
+dimer.mo_coeff = basis_utils.noscfbasis(dimer,bottom,top)
+rt_scf = rt_scf.RT_SCF(dimer, 0.2, 500)
+rt_scf.observables.update(charge=True)
+
+# We'll use the input_fragments function here so that the Mulliken charges on the monomers are calculated
+rt_utils.input_fragments(rt_scf,bottom,top)
+
+# Run dynamics
+rt_scf.kernel()
--- examples/NWCHEM_Recreations/TCNE_ChargeTransfer/Density_Fitted/Workup_TCNE_ChargeTransfer.py
+++ examples/NWCHEM_Recreations/TCNE_ChargeTransfer/Density_Fitted/Workup_TCNE_ChargeTransfer.py
@@ -5,9 +5,9 @@ result = parse_output('TCNE_ChargeTransfer.pyo')
 
 
 plt.figure()
-plt.plot(result['time'], result['frag_charge'][:,0], label='Bottom')
-plt.plot(result['time'], result['frag_charge'][:,1], label='Top')
+plt.plot(result['time'] / 41.34, -1 * result['frag_charge'][:,0], label='Bottom')
+plt.plot(result['time'] / 41.34, -1 * result['frag_charge'][:,1], label='Top')
 plt.ylabel('Charge')
-plt.xlabel('Time (au)')
+plt.xlabel('Time (fs)')
 plt.legend()
 plt.savefig('TCNE_ChargeTransfer_Charges.png', bbox_inches='tight')
