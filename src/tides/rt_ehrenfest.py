Changed int() calls to round() calls to avoid numerical instabilities with roundoff errors. Removed (commented out) bug in magnus interpol which shifted time-dependent potentials forwards by 1 time step.

src/tides/rt_ehrenfest.py
@@ -38,7 +38,7 @@ class RT_Ehrenfest(RT_SCF):
         elif self._scf.istype('UHF'): self._grad_func = grad.UHF
         elif self._scf.istype('GKS'): self._grad_func = grad.UKS # grad.GKS doesn't exist
         elif self._scf.istype('GHF'): self._grad_func = grad.UHF # grad.GHF doesn't exist
-        
+
         # Reminder to check if forces should be updated again after excite()
         self.nuc.force = ehrenfest_force.get_force(self)
 
@@ -48,7 +48,7 @@ class RT_Ehrenfest(RT_SCF):
             self.get_mo_coeff_print = get_mo_coeff_print
 
     def update_time(self):
-        current_N = np.mod(int(self.current_time / self.timestep), self.Ne_step * self.N_step)
+        current_N = np.mod(round(self.current_time / self.timestep), self.Ne_step * self.N_step)
         current_Ne = np.mod(current_N, self.Ne_step)
         if current_N == 0: # k = 0, j = 0
             self.nuc.update_vel(0.5 * self.N_step * self.Ne_step * self.timestep)
@@ -66,7 +66,7 @@ class RT_Ehrenfest(RT_SCF):
             self.update_mol()
 
         self.current_time += self.timestep
-   
+
     def update_force(self):
         self.nuc.update_vel(-0.5 * self.N_step * self.Ne_step * self.timestep)
         self.nuc.force = ehrenfest_force.get_force(self)

src/tides/rt_integrators.py
@@ -19,7 +19,7 @@ def magnus_step(rt_scf):
     u = expm(-1j*2*rt_scf.timestep*fock_orth)
 
     mo_coeff_orth_new = np.matmul(u, rt_scf.mo_coeff_orth_old)
-    
+
     rt_scf.mo_coeff_orth_old = rt_scf.rotate_coeff_to_orth(rt_scf._scf.mo_coeff)
     rt_scf._scf.mo_coeff = rt_scf.rotate_coeff_to_ao(mo_coeff_orth_new)
     rt_scf.den_ao = rt_scf._scf.make_rdm1(mo_occ=rt_scf.occ)
@@ -38,7 +38,7 @@ def magnus_interpol(rt_scf):
 
     mo_coeff_orth = rt_scf.rotate_coeff_to_orth(rt_scf._scf.mo_coeff)
     fock_orth_p12dt = 2 * rt_scf._fock_orth - rt_scf._fock_orth_n12dt
-    
+
     # Update time, mol is updated here if rt_scf is an Ehrenfest obj
     rt_scf.update_time()
 
@@ -49,9 +49,9 @@ def magnus_interpol(rt_scf):
         mo_coeff_ao_pdt = rt_scf.rotate_coeff_to_ao(mo_coeff_orth_pdt)
         den_ao_pdt = rt_scf._scf.make_rdm1(mo_coeff=mo_coeff_ao_pdt,
                                           mo_occ=rt_scf.occ)
-        rt_scf.current_time += rt_scf.timestep
+        #rt_scf.current_time += rt_scf.timestep
         fock_orth_pdt = rt_scf.get_fock_orth(den_ao_pdt)
-        rt_scf.current_time -= rt_scf.timestep
+        #rt_scf.current_time -= rt_scf.timestep
 
         if (iteration > 0 and
         abs(np.linalg.norm(den_ao_pdt)